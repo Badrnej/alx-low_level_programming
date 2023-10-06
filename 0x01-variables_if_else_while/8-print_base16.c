af2583154d3e392a598f85827b350726fadbcd10
badrnej <nejaa.badr@gmail.com>
2023-10-06 10:50:33 -0700

    test8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@
 
 int main(void)
 {
-	int num - 48; /*48; decimal rep of 0*/
+	int num = 48; /*48; decimal rep of 0*/
 
 	while (num <= 102) /*102;  decimal rep of f*/
 	{
-		putchar(num):
+		putchar(num);
 
 		/* after 9 we jump till 96; */
 		if (num == 57)
