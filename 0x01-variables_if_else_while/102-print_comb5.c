3fcdb6bfd0db0d8519c9977857af73dabd7ddbfd
badrnej <nejaa.badr@gmail.com>
2023-10-06 12:26:47 -0700

    test12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: write a programe that print all possible
+ *		different combinations of tow num
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num1 = 0, num2;
+
+	while (num1 <= 99)
+	{
+		num2 = num1;
+		while (num2 <= 99)
+		{
+			if (num2 != num1)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
