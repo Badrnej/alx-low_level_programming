d07c06f92809ee2974cf8eed4065f60b926e97ac
badrnej <nejaa.badr@gmail.com>
2023-10-11 19:29:04 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,12 +5,8 @@
 * Return: 0
 */
 
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+int main(void) {
+    int i = 0;
+    positive_or_negative(i);
+    return 0;
 }
