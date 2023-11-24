5159b193fce5ac97e0ada765fa55bbee671e81ed
Badrnej <nejaa.badr@gmail.com>
2023-11-23 20:33:04 -0800

    B1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,7 +5,7 @@
  * @base: base of the exponent
  * @power: power of the exponent
  * Return: value of (base ^ power)
-**/
+ **/
 
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
@@ -19,11 +19,11 @@ unsigned long int _pow(unsigned int base, unsigned int power)
 }
 
 /**
- *  * print_binary - prints a number in binary notation
- *   * @n: number to print
- *    *
- *     * Return: void
- *      */
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ * Return: void
+ **/
+
 void print_binary(unsigned long int n)
 {
 	unsigned long int divisor, check;
