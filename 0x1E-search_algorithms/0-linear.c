02cb5e044ba8ad4304e60615060c79bf6d7a4e03
Badrnej <nejaa.badr@gmail.com>
2024-05-08 17:08:02 -0700

    ookk

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,8 @@
 #include "search_algos.h"
 
 /**
- * linear_search - function that searches for a value in an array of integers
- * @array: pointer to the first element of the array to search in
+ * linear_search - that searches for a value in an array 
+ * @array: pointer to 1st element of the array to search in
  * @size: number of elements in array
  * @value: the value to search for
  * Return: first index where value is located
